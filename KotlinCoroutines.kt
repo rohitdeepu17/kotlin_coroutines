a1ba0cbd8ac0c8395d2576848aa58ea5d5a1feb7
Rohit Singla <rohitdeepu17@gmail.com>
2023-08-03 11:10:49 +0530

    use value returned by suspending function

--- KotlinCoroutines.kt
+++ KotlinCoroutines.kt
@@ -14,7 +14,8 @@ fun main() = runBlocking {//Main Thread
 
         val jobDeferred: Deferred<Int> = async{     //Thread main
             println("Fake work starts at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")  //Thread main
-            mySuspendFun(1000)     //does not block the thread on which it is running
+            val suspendFunReturnVal = mySuspendFun(1000)     //does not block the thread on which it is running
+            println("The value returned by mySuspendFun is $suspendFunReturnVal at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")
             println("Fake work finished at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")     //Either main thread or some other thread
             15
         }
@@ -25,15 +26,17 @@ fun main() = runBlocking {//Main Thread
         println("Main program ends at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")
 }
 
-suspend fun mySuspendFun(timeToDelay: Long){
+suspend fun mySuspendFun(timeToDelay: Long): String{
     delay(timeToDelay)
+    return "some value from mySuspendFun"
 }
 
 /*
 OUTPUT:
 main program starts at 0: main
-Fake work starts at 12: main
-Fake work finished at 1021: main
-The value returned from coroutine is 15 at 1026: main
-Main program ends at 1026: main
+Fake work starts at 10: main
+The value returned by mySuspendFun is some value from mySuspendFun at 1025: main
+Fake work finished at 1027: main
+The value returned from coroutine is 15 at 1030: main
+Main program ends at 1030: main
  */
