91e856b8c4e3c31c644bdd22d1a49ba0a10a6d10
Rohit Singla <rohitdeepu17@gmail.com>
2023-08-03 11:04:29 +0530

    let the async coroutine return an integer

--- KotlinCoroutines.kt
+++ KotlinCoroutines.kt
@@ -12,13 +12,15 @@ fun main() = runBlocking {//Main Thread
         val startTime = System.currentTimeMillis()
         println("main program starts at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")
 
-        val jobDeferred: Deferred<Unit> = async{     //Thread main
+        val jobDeferred: Deferred<Int> = async{     //Thread main
             println("Fake work starts at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")  //Thread main
             mySuspendFun(1000)     //does not block the thread on which it is running
             println("Fake work finished at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")     //Either main thread or some other thread
+            15
         }
 
-        jobDeferred.join()          //wait for the coroutine to finish it's job
+        val num:Int = jobDeferred.await()          //wait for the coroutine to finish it's job and return result
+        println("The value returned from coroutine is $num at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")
 
         println("Main program ends at ${System.currentTimeMillis()-startTime}: ${Thread.currentThread().name}")
 }
@@ -30,7 +32,8 @@ suspend fun mySuspendFun(timeToDelay: Long){
 /*
 OUTPUT:
 main program starts at 0: main
-Fake work starts at 11: main
-Fake work finished at 1027: main
-Main program ends at 1030: main
+Fake work starts at 12: main
+Fake work finished at 1021: main
+The value returned from coroutine is 15 at 1026: main
+Main program ends at 1026: main
  */
